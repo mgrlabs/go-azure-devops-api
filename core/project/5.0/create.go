e82c8339d034bbe63c8217421d92d120c67fcd91
Mark Revill <mark.revill@vibrato.com.au>
2019-02-12 13:12:19 +1100

    Added base64 encoding of PAT

--- core/project/5.0/create.go
+++ core/project/5.0/create.go
@@ -1,7 +1,8 @@
-package azuredevopsapi
+package devopsprojectcreate
 
 import (
 	"bytes"
+	b64 "encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -19,17 +20,20 @@ type ProjectResponse struct {
 	URL    string `json:"url"`
 }
 
+// Comment
 func CreateProject(pat string, organization string, projectName string) ProjectResponse {
+
+	patEncoded := b64.StdEncoding.EncodeToString([]byte(":" + pat))
 
 	var jsonFormat = "{ \"name\": \"" + projectName + "\", \"description\": \"Frabrikam travel app for Windows Phone\", \"capabilities\": { \"versioncontrol\": { \"sourceControlType\": \"Git\"}, \"processTemplate\": {  \"templateTypeId\": \"6b724908-ef14-45cf-84f8-768b5384da45\" }}}"
 
 	var jsonStr = []byte(jsonFormat)
 
-	var baseUrl = "https://dev.azure.com/" + organization + "/_apis/projects?api-version=5.0"
+	var baseURL = "https://dev.azure.com/" + organization + "/_apis/projects?api-version=5.0"
 
-	req, err := http.NewRequest("POST", baseUrl, bytes.NewBuffer(jsonStr))
+	req, err := http.NewRequest("POST", baseURL, bytes.NewBuffer(jsonStr))
 
-	basic := "Basic " + pat
+	basic := "Basic " + patEncoded
 
 	req.Header.Set("Authorization", basic)
 	req.Header.Set("Content-Type", "application/json")
